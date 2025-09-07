6a2874897ff7de7db1e0ab9fcdefa77252b178a1
bugname7 <valisher2106@gmail.com>
2025-09-07 14:57:50 +0500

    likes page update

---
+++ src/app/likes/page.tsx
@@ -0,0 +1,8 @@
+export default function LikesPage() {
+    return (
+        <main className="p-6">
+            <h1 className="text-2xl font-bold">❤️ Sevimlilar</h1>
+            <p>Bu yerda siz yoqtirgan mahsulotlaringiz chiqadi.</p>
+        </main>
+    );
+}
