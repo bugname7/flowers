7089a5a09ad9f8b600522f2369865763a34d5e3d
bugname7 <valisher2106@gmail.com>
2025-09-08 18:10:21 +0500

    clear scrool

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,16 +2,17 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="bg-black text-center py-6 mt-12">
+    <footer className="bg-black text-center py-6 mt-12 overflow-hidden">
       <div className="mb-4 flex justify-center" data-aos="zoom-out">
         <Link href={'/'}>
           <img
             src="/images/logo-image.png"
             alt="logo image"
-            className="w-32 sm:w-36 md:w-40 object-contain"
-          /></Link>
+            className="w-24 sm:w-28 md:w-32 lg:w-40 max-w-full h-auto"
+          />
+        </Link>
       </div>
-      <p className="text-sm text-pink-500 font-mono" >
+      <p className="text-sm text-pink-500 font-mono">
         © {new Date().getFullYear()} Florist Diyora. Barcha huquqlar himoyalangan.
       </p>
     </footer>
