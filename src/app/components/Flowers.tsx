659ca0103d79677dc9e810d4b07908d117a708be
bugname7 <valisher2106@gmail.com>
2025-09-11 10:38:53 +0500

    flowers page update

--- src/app/components/Flowers.tsx
+++ src/app/components/Flowers.tsx
@@ -1,164 +1,117 @@
 import Image from "next/image"
 
 const Flowers = () => {
-    return (
-        <div className="overflow-x-hidden">
-            {/* Birinchi blok */}
-            <div className="container mx-auto w-full flex flex-row gap-6 flex-wrap">
-                <div className="flex flex-row gap-4 flex-1 min-w-[300px]">
-                    {/* Katta rasm */}
-                    <div className="w-full h-[380px]" data-aos="fade-right">
-                        <Image
-                            src="/images/white-arx.jpg"
-                            alt="white arxideya"
-                            width={600}
-                            height={400}
-                            className="animate-slide-in-from-left w-full h-full rounded-xl object-cover shadow-md"
-                        />
-                    </div>
+  return (
+    <div className="overflow-x-hidden overflow-y-hidden">
+      {/* Birinchi blok */}
+      <div className="container mx-auto max-w-full flex flex-row gap-6 flex-wrap">
+        <div className="flex flex-row gap-4 flex-1 min-w-[300px]">
+          {/* Katta rasm */}
+          <div className="relative w-full h-[380px]" data-aos="fade-right">
+            <Image
+              src="/images/white-arx.jpg"
+              alt="white arxideya"
+              fill
+              className="animate-slide-in-from-left rounded-xl object-cover shadow-md"
+            />
+          </div>
 
-                    {/* Ikki kichik rasm */}
-                    <div className="flex flex-col gap-4 basis-1/2 h-[365px]">
-                        <div className="h-1/2" data-aos="fade-up">
-                            <Image
-                                src="/images/lola.jpg"
-                                alt="lola image"
-                                width={200}
-                                height={100}
-                                className="w-full h-full rounded-lg shadow-md object-cover"
-                            />
-                        </div>
-                        <div className="h-1/2" data-aos="fade-up-right">
-                            <Image
-                                src="/images/sakura.jpg"
-                                alt="sakura image"
-                                width={200}
-                                height={100}
-                                className="w-full h-full rounded-lg shadow-md object-cover"
-                            />
-                        </div>
-                    </div>
-                </div>
-
-                {/* Matn qismi */}
-                <div className="flex-1 text-justify min-w-[300px]">
-                    <p
-                        className="font-mono leading-7 mb-2 sm:mb-8 font-medium"
-                        data-aos="zoom-in-left"
-                    >
-                        🌸 Biz gullarni Farg‘ona viloyati, Quva tumanida uy sharoitida
-                        yasaladi va sotamiz!
-                    </p>
-                    <p
-                        className="font-mono leading-6 font-medium mb-6"
-                        data-aos="fade-right"
-                    >
-                        🌸 Buyurtma berish uchun qulay va professional onlayn-do‘konimizdan
-                        foydalanishingiz mumkin. Do‘konimiz barcha asosiy onlayn to‘lov
-                        usullarini qo‘llab-quvvatlaydi, va biz guldastalarni aniq tasvirlab,
-                        suratga olib taqdim etamiz.
-                    </p>
-                    <p
-                        className="font-mono leading-7 mb-2 sm:mb-8 font-medium"
-                        data-aos="zoom-in-left"
-                    >
-                        🌸 Buyurtmalarni oz vaqtida qilib berishga harakat qilamiz
-                    </p>
-                    <p
-                        className="font-mono leading-7 mb-6 font-medium"
-                        data-aos="fade-right"
-                    >
-                        🌸 Har bir guldasta ishtiyoq bilan yaratiladi va maxsus tanlangan
-                        gullardan yig‘iladi. Biz mijozlarimiz bilan yaxshi aloqani,
-                        xushmuomalalikni va buyurtmalarning sifatli bajarilishini
-                        kafolatlaymiz. Shuningdek, anonimlik va yordam ham ta’minlanadi.
-                    </p>
-                </div>
+          {/* Ikki kichik rasm */}
+          <div className="flex flex-col gap-4 basis-1/2 h-[365px]">
+            <div className="relative h-1/2" data-aos="fade-up">
+              <Image
+                src="/images/lola.jpg"
+                alt="lola image"
+                fill
+                className="rounded-lg shadow-md object-cover"
+              />
             </div>
-
-            {/* O‘rta matn */}
-            <div data-aos="zoom-out-right">
-                <h4 className="font-mono leading-7 mb-6 font-medium">
-                    🌸 Siz Quvada guldastaga ishonch bilan buyurtma berishingiz mumkin,
-                    chunki biz sotib olingan gullardan imkon qadar uzoq vaqt zavq
-                    olishingiz uchun gul tuzilmalarimizning yuqori sifati, yangiligi va
-                    mustahkamligiga kafolat beramiz.
-                </h4>
+            <div className="relative h-1/2" data-aos="fade-up-right">
+              <Image
+                src="/images/sakura.jpg"
+                alt="sakura image"
+                fill
+                className="rounded-lg shadow-md object-cover"
+              />
             </div>
-
-            {/* Ikkinchi blok */}
-            <div className="container mx-auto flex flex-row gap-6 flex-wrap">
-                {/* Chap tarafdagi matn */}
-                <div className="flex-1 text-justify min-w-[300px]">
-                    <h3
-                        className="font-mono leading-7 text-xl mb-2 sm:mb-8 font-bold"
-                        data-aos="fade-right"
-                    >
-                        🌸 Nima uchun bizning gul do'konimizdan foydalanasiz?
-                    </h3>
-                    <p
-                        className="font-mono leading-6 font-medium mb-6"
-                        data-aos="fade-left"
-                    >
-                        🌸 Buyurtma berish uchun qulay va professional onlayn-do‘konimizdan
-                        foydalanishingiz mumkin. Do‘konimiz barcha asosiy onlayn to‘lov
-                        usullarini qo‘llab-quvvatlaydi, va biz guldastalarni aniq tasvirlab,
-                        suratga olib taqdim etamiz.
-                    </p>
-                    <p
-                        className="font-mono leading-7 mb-2 sm:mb-8 font-medium"
-                        data-aos="fade-right"
-                    >
-                        🌸 Buyurtmalarni telefon, elektron pochta orqali qabul qilamiz.
-                    </p>
-                    <p
-                        className="font-mono leading-7 mb-6 font-medium"
-                        data-aos="fade-left"
-                    >
-                        🌸 Har bir guldasta ishtiyoq bilan yaratiladi va maxsus tanlangan
-                        gullardan yig‘iladi. Biz mijozlarimiz bilan yaxshi aloqani,
-                        xushmuomalalikni va buyurtmalarning sifatli bajarilishini
-                        kafolatlaymiz. Shuningdek, anonimlik va yordam ham ta’minlanadi.
-                    </p>
-                </div>
-
-                {/* O‘ng tarafdagi rasmlar */}
-                <div className="flex flex-row gap-4 flex-1 sm:min-w-[300px]">
-                    <div className="flex flex-col gap-4 basis-1/2 h-[365px]">
-                        <div className="h-1/2" data-aos="zoom-in-right">
-                            <Image
-                                src="/images/lola1.jpg"
-                                alt="lola1 image"
-                                width={200}
-                                height={100}
-                                className="w-full h-full rounded-lg shadow-md object-cover"
-                            />
-                        </div>
-                        <div className="h-1/2" data-aos="zoom-out">
-                            <Image
-                                src="/images/znak.jpg"
-                                alt="znak image"
-                                width={200}
-                                height={100}
-                                className="w-full h-full rounded-lg shadow-md object-cover"
-                            />
-                        </div>
-                    </div>
-
-                    <div className="basis-1/2 h-[380px]" data-aos="zoom-in-left">
-                        <Image
-                            src="/images/arxideya.jpg"
-                            alt="arxideya"
-                            width={600}
-                            height={400}
-                            className="w-full h-full rounded-xl object-cover shadow-md"
-                        />
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
-    )
+
+        {/* Matn qismi */}
+        <div className="flex-1 text-justify min-w-[300px]">
+          <p className="font-mono leading-7 mb-2 sm:mb-8 font-medium" data-aos="zoom-in-left">
+            🌸 Biz gullarni Farg‘ona viloyati, Quva tumanida uy sharoitida yasaladi va sotamiz!
+          </p>
+          <p className="font-mono leading-6 font-medium mb-6" data-aos="fade-right">
+            🌸 Buyurtma berish uchun qulay va professional onlayn-do‘konimizdan foydalanishingiz mumkin. Do‘konimiz barcha asosiy onlayn to‘lov usullarini qo‘llab-quvvatlaydi, va biz guldastalarni aniq tasvirlab, suratga olib taqdim etamiz.
+          </p>
+          <p className="font-mono leading-7 mb-2 sm:mb-8 font-medium" data-aos="zoom-in-left">
+            🌸 Buyurtmalarni oz vaqtida qilib berishga harakat qilamiz
+          </p>
+          <p className="font-mono leading-7 mb-6 font-medium" data-aos="fade-right">
+            🌸 Har bir guldasta ishtiyoq bilan yaratiladi va maxsus tanlangan gullardan yig‘iladi. Biz mijozlarimiz bilan yaxshi aloqani, xushmuomalalikni va buyurtmalarning sifatli bajarilishini kafolatlaymiz. Shuningdek, anonimlik va yordam ham ta’minlanadi.
+          </p>
+        </div>
+      </div>
+
+      {/* O‘rta matn */}
+      <div data-aos="zoom-out-right" className="container mx-auto max-w-full my-6">
+        <h4 className="font-mono leading-7 mb-6 font-medium">
+          🌸 Siz Quvada guldastaga ishonch bilan buyurtma berishingiz mumkin, chunki biz sotib olingan gullardan imkon qadar uzoq vaqt zavq olishingiz uchun gul tuzilmalarimizning yuqori sifati, yangiligi va mustahkamligiga kafolat beramiz.
+        </h4>
+      </div>
+
+      {/* Ikkinchi blok */}
+      <div className="container mx-auto max-w-full flex flex-row gap-6 flex-wrap">
+        {/* Chap tarafdagi matn */}
+        <div className="flex-1 text-justify min-w-[300px]">
+          <h3 className="font-mono leading-7 text-xl mb-2 sm:mb-8 font-bold" data-aos="fade-right">
+            🌸 Nima uchun bizning gul do'konimizdan foydalanasiz?
+          </h3>
+          <p className="font-mono leading-6 font-medium mb-6" data-aos="fade-left">
+            🌸 Buyurtma berish uchun qulay va professional onlayn-do‘konimizdan foydalanishingiz mumkin. Do‘konimiz barcha asosiy onlayn to‘lov usullarini qo‘llab-quvvatlaydi, va biz guldastalarni aniq tasvirlab, suratga olib taqdim etamiz.
+          </p>
+          <p className="font-mono leading-7 mb-2 sm:mb-8 font-medium" data-aos="fade-right">
+            🌸 Buyurtmalarni telefon, elektron pochta orqali qabul qilamiz.
+          </p>
+          <p className="font-mono leading-7 mb-6 font-medium" data-aos="fade-left">
+            🌸 Har bir guldasta ishtiyoq bilan yaratiladi va maxsus tanlangan gullardan yig‘iladi. Biz mijozlarimiz bilan yaxshi aloqani, xushmuomalalikni va buyurtmalarning sifatli bajarilishini kafolatlaymiz. Shuningdek, anonimlik va yordam ham ta’minlanadi.
+          </p>
+        </div>
+
+        {/* O‘ng tarafdagi rasmlar */}
+        <div className="flex flex-row gap-4 flex-1 sm:min-w-[300px]">
+          <div className="flex flex-col gap-4 basis-1/2 h-[365px]">
+            <div className="relative h-1/2" data-aos="zoom-in-right">
+              <Image
+                src="/images/lola1.jpg"
+                alt="lola1 image"
+                fill
+                className="rounded-lg shadow-md object-cover"
+              />
+            </div>
+            <div className="relative h-1/2" data-aos="zoom-out">
+              <Image
+                src="/images/znak.jpg"
+                alt="znak image"
+                fill
+                className="rounded-lg shadow-md object-cover"
+              />
+            </div>
+          </div>
+
+          <div className="relative basis-1/2 h-[380px]" data-aos="zoom-in-left">
+            <Image
+              src="/images/arxideya.jpg"
+              alt="arxideya"
+              fill
+              className="rounded-xl object-cover shadow-md"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Flowers
-    
--- src/app/components/Map.tsx
+++ src/app/components/Map.tsx
@@ -1,18 +1,18 @@
 import Image from "next/image";
 
 export default function Map() {
-    return (
-        <div className="w-full bg-gray-100 flex flex-col md:flex-row items-center justify-center py-6 px-4 mt-20">
+    return ( 
+        <div className="w-full flex flex-col md:gap-14 md:flex-row items-center justify-center py-6 px-4 mt-20">
 
-            <div className="w-full md:w-1/2 h-[300px] md:h-[500px] relative" data-aos="fade-right"
+            <div className="w-full md:w-1/3 h-[300px] md:h-[500px] relative" data-aos="fade-right"
                 data-aos-offset="300"
                 data-aos-easing="ease-in-sine">
 
-                <iframe allowFullScreen src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d6066.0907624694055!2d72.01807111501688!3d40.51848801224343!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sQo&#39;shkechik%20qishlog&#39;i%20murotkosa!5e0!3m2!1sru!2s!4v1757330478116!5m2!1sru!2s" className="w-full h-full rounded-lg shadow-md border-0" loading="lazy" ></iframe>
+                <iframe allowFullScreen src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d6066.0907624694055!2d72.01807111501688!3d40.51848801224343!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sQo&#39;shkechik%20qishlog&#39;i%20murotkosa!5e0!3m2!1sru!2s!4v1757330478116!5m2!1sru!2s" className="w-full h-full rounded-lg shadow-xl border-0" loading="lazy" ></iframe>
             </div>
 
             <div
-                className="w-full md:w-1/3 bg-white shadow-xl p-6 md:p-8 mt-6 md:mt-0 md:ml-6 cursor-pointer "
+                className="w-full md:w-1/4 bg-white shadow-xl p-6 md:p-8 mt-6 md:mt-0 md:ml-6 cursor-pointer "
                 data-aos="flip-left"
                 data-aos-easing="ease-out-cubic"
                 data-aos-duration="2000"
@@ -25,10 +25,10 @@ export default function Map() {
                         height={80}
                     />
                     <h2 className="mt-2 text-lg md:text-xl font-semibold font-mono">
-                      🌸  FLOWERS-Diyora
+                        🌸  FLOWERS-Diyora
                     </h2>
                     <p className="text-gray-500 text-sm md:text-base font-mono">
-                    🌸 Eng jozibali gullar 
+                        🌸 Eng jozibali gullar
                     </p>
                 </div>
 
